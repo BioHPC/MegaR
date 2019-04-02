dc2e1392f7712c09e503a756b618d0c03d7d4ea9
DEliza <32403146+Elizaddh@users.noreply.github.com>
2019-04-02 11:56:37 -0500

    Update prediction.R

--- R/prediction.R
+++ R/prediction.R
@@ -5,12 +5,11 @@
 #' @param unknormdata unknown dataset
 #' @param a model list with elements ******
 getunknpredict <- function(unknormdata, a){
-  #unknormdata <- getGoodfeature(getLevelDataM(x,level), threshold, samplePercent , normval )
-  
-  new_leveled_ukn.s1 <- unknormdata[match(rownames(t(a[[2]])) , rownames(unknormdata)), ] ### TO DO
-  new_leveled_ukn.s1 <- t(new_leveled_ukn.s1)
+   rownames(unknormdata)<- gsub("|", ".", rownames(unknormdata), fixed = TRUE)
+  new_leveled_ukn.s1 <- unknormdata[match(colnames(a[[1]]) , rownames(unknormdata)),  ] 
+  rownames(new_leveled_ukn.s1) <- colnames(a[[1]])
   new_leveled_ukn.s1[is.na(new_leveled_ukn.s1)] <- 0
   #prediction of unknown data
-  RF.predict <- predict(a[[3]] , new_leveled_ukn.s1)
+  RF.predict <- predict(a[[3]] , t(new_leveled_ukn.s1))
   return(RF.predict)
 }
