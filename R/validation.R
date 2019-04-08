579c2b8c86098a530a8870430d57821e55eaf235
DEliza <32403146+Elizaddh@users.noreply.github.com>
2019-04-08 07:18:19 -0500

    Update validation.R

--- R/validation.R
+++ R/validation.R
@@ -1,4 +1,6 @@
-validation <- function(N,mytable3, classid, sampleid, ruleout, psd, metadat ){  
+
+
+validation <- function(N,  modelclas ,mytable3, classid, sampleid, ruleout, psd, metadat ){  
   otu_table_scaled <- mytable3
   otu_table_scaled_state <- data.frame(t(otu_table_scaled))  
   #otu_table_scaled_state$country <- mymetadata()[,input$classid][match(str_remove(rownames(otu_table_scaled_state), "_profile"), mymetadata()[,input$sampleid])]  
@@ -10,16 +12,28 @@ validation <- function(N,mytable3, classid, sampleid, ruleout, psd, metadat ){
   
   Acc3<- NULL
   Kpp3 <- NULL
-for (i in 1:N) {
-  smp_size <- floor((psd/100) * nrow(otu_table_scaled_state1))
-  train_ind <- sample(seq_len(nrow(otu_table_scaled_state1)), size = smp_size)
-  train <- otu_table_scaled_state1[train_ind, ]
-  train<-droplevels(train)
-  test <- otu_table_scaled_state1[-train_ind,]
-  RF_state_classify <- randomForest::randomForest(as.factor(country)~. , data =train, importance = T, proximities = T)
-RF_state_classify_loocv <- train(as.factor(country)~. , data =train , method="rf",ntree= 501,  tuneGrid=data.frame( mtry=RF_state_classify$mtry ), trControl=fit_control)
-Acc3[i] <- RF_state_classify_loocv$results$Accuracy
-Kpp3[i] <- RF_state_classify_loocv$results$Kappa
-}
+  for (i in 1:N) {
+    smp_size <- floor((psd/100) * nrow(otu_table_scaled_state1))
+    train_ind <- sample(seq_len(nrow(otu_table_scaled_state1)), size = smp_size)
+    train <- otu_table_scaled_state1[train_ind, ]
+    train<-droplevels(train)
+    test <- otu_table_scaled_state1[-train_ind,]
+    if(modelclas == "rfmodel"){
+      RF_state_classify <- randomForest::randomForest(as.factor(country)~. , data =train, importance = T, proximities = T)
+      RF_state_classify_loocv <- train(as.factor(country)~. , data =train , method="rf",ntree= 501,  tuneGrid=data.frame( mtry=RF_state_classify$mtry ), trControl=fit_control)
+      Acc3[i] <- RF_state_classify_loocv$results$Accuracy
+      Kpp3[i] <- RF_state_classify_loocv$results$Kappa
+    }
+    else if(modelclas == "svmmodel"){
+      RF_state_classify_loocv <- train(as.factor(country)~. , data =train , method="svm", trControl=fit_control)
+      Acc3[i] <- RF_state_classify_loocv$results$Accuracy
+      Kpp3[i] <- RF_state_classify_loocv$results$Kappa
+    }
+    else {
+      RF_state_classify_loocv <- train(as.factor(country)~. , data =train , method="glm"  , trControl=fit_control)
+      Acc3[i] <- RF_state_classify_loocv$results$Accuracy
+      Kpp3[i] <- RF_state_classify_loocv$results$Kappa
+    }
+  }
   sprintf("The 10 fold cross validated obtained from the average of %i independent run is  %f. ", N , sum(Acc3)/N )
 }
