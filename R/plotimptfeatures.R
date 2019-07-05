56f2e25ad15df9ed934da7daf41d7890dca1c08d
DEliza <32403146+Elizaddh@users.noreply.github.com>
2019-07-05 15:43:03 -0500

    Update plotimptfeatures.R

--- R/plotimptfeatures.R
+++ R/plotimptfeatures.R
@@ -7,20 +7,14 @@
 #' @export
 #'
 plotimptfeatures <- function(RF_state_classify, noOffeature){
-    fc<- grDevices::colorRampPalette(rev(brewer.pal(n = 9, "Blues")))
-    RF_state_classify_imp <- as.data.frame( RF_state_classify$importance )
-    with(RF_state_classify_imp,{
-    RF_state_classify_imp$features <- rownames( RF_state_classify_imp )
-    RF_state_classify_imp_sorted <- plyr::arrange( RF_state_classify_imp  ,
-                        plyr::desc(MeanDecreaseAccuracy))
-    a <- graphics::barplot(RF_state_classify_imp_sorted[1:10,
-                                                        "MeanDecreaseAccuracy"],
-            names.arg=stringr::str_replace(RF_state_classify_imp_sorted[
-                1:10,"features"],
-                                   "X", "OTU_") ,
-            ylab="Mean Decrease in Accuracy", las=2, ylim=c(0,0.015),
-            main="Top ten important variables for classification",
-            cex.axis = .75, cex.names = .75, col= fc(10))
-    })
-    return(a)
+lk<- varImp(RF_state_classify)
+tablelk<-lk$importance
+tablelk$first <- rownames(tablelk)
+new <-tablelk[order(tablelk$Overall, decreasing = TRUE),]
+fc <- grDevices::colorRampPalette(rev(RColorBrewer::brewer.pal(n = 9, 
+                                                              "Blues")))
+a <- graphics::barplot(new[1:10,]$Overall,names.arg= substr(stringr::str_remove
+                                             (new[1:10,]$first, ".*s__" ),1,20),
+        cex.axis = .75, cex.names = .75, col = fc(10))
+return(a)
 }
