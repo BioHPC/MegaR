6a3480eba271051533bd21c13196dfa94c57c185
DEliza <32403146+Elizaddh@users.noreply.github.com>
2020-09-21 12:29:59 -0500

    Update getGoodfeature.R
    Added dependent function

--- R/getGoodfeature.R
+++ R/getGoodfeature.R
@@ -9,6 +9,19 @@
 #'  the threshold amount of value in its feature
 #' @param normval wheather cumulative sum normalization is to be used or not
 #' @export
+quantile_normalisation <- function(df){
+    df_rank <- apply(df,2,rank,ties.method="min")
+    df_sorted <- data.frame(apply(df, 2, sort))
+    df_mean <- apply(df_sorted, 1, mean)
+    
+    index_to_mean <- function(my_index, my_mean){
+        return(my_mean[my_index])
+    }
+    
+    df_final <- apply(df_rank, 2, index_to_mean, my_mean=df_mean)
+    rownames(df_final) <- rownames(df)
+    return(df_final)
+}
 
 getGoodfeature <- function(alltable2, threshold, samplePercent, normval){
     row2keep <- c()
